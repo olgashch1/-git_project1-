f45fa1e9ca1c3a61ef48ef47f4411ac8e3912e43
User <iktinf@gmail.com>
2024-01-10 19:48:27 +0300

    Second commit

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')
     print('And bye')# Press Ctrl+F8 to toggle the breakpoint.
-
+    print('krach')
+    print('krach')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
