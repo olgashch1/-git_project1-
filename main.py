3bab51648ef4be4aef25a9f75b367a3132f58601
User <iktinf@gmail.com>
2024-01-10 20:54:41 +0300

    Other commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def print_hi(name):
     print('And bye')# Press Ctrl+F8 to toggle the breakpoint.
     print('krach')
     print('krach')
+    print('Commit')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
